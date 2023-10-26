c33e6cf90ad8ff18ffa83bdee42ba5a27c25057a
cohsh <82075327+cohsh@users.noreply.github.com>
2023-10-26 22:08:41 +0900

    :bug: + :recycle: Error handling in electron.free

--- elphem/electron/free.py
+++ elphem/electron/free.py
@@ -9,30 +9,55 @@ class FreeElectron:
     electron_per_cell: int
     
     def __post_init__(self):
+        if not isinstance(self.lattice, EmptyLattice):
+            raise TypeError("The type of first variable must be EmptyLattice.")
+        if self.electron_per_cell <= 0:
+            raise ValueError("Second variable (number of electrons per unit cell) should be a positive value.")
+        
         self.electron_density = self.electron_per_cell / self.lattice.volume["primitive"]
 
     def fermi_energy(self) -> float:
-        fermi_energy = 0.5 * (3 * np.pi ** 2 * self.electron_density) ** (2/3)
-        return fermi_energy
+        """
+        Get Fermi energy.
+        
+        Return
+            Fermi energy
+        """
+        return 0.5 * (3 * np.pi ** 2 * self.electron_density) ** (2/3)
 
     def eigenenergy(self, k: np.ndarray) -> np.ndarray:
-        eigenenergy = 0.5 * np.linalg.norm(k, axis=k.ndim-1) ** 2 - self.fermi_energy()
-        return eigenenergy
-
-    @staticmethod    
-    def velocity(k: np.ndarray) -> np.ndarray:
-        velocity = k
-        return velocity
+        """
+        Get electron eigenenergies.
+        
+        Args
+            k: A numpy array representing vectors in reciprocal space.
+        
+        Return
+            Electron eigenenergies
+        """
+        return 0.5 * np.linalg.norm(k, axis=k.ndim-1) ** 2 - self.fermi_energy()
     
-    def grid(self, n_g: np.ndarray, n_k: np.ndarray) -> np.ndarray:
+    def grid(self, n_g: np.ndarray, n_k: np.ndarray) -> tuple:
+        """
+        Get (G, k)-grid.
+        
+        Args
+            n_g: A numpy array representing the dense of G-grid in reciprocal space.
+            n_k: A numpy array representing the dense of k-grid in reciprocal space.
+            
+        Return
+            A tuple containing:
+                G-meshgrid
+                k-meshgrid
+        """
         basis = self.lattice.basis["reciprocal"]
         
-        grid = np.meshgrid(*[np.arange(-i, i) for i in n_g], *[np.linspace(-0.5, 0.5, i) for i in n_k])
-        grid = np.array(grid)
+        # Combining the grid generation for G and k
+        grid_points = [np.arange(-i, i) for i in n_g] + [np.linspace(-0.5, 0.5, i) for i in n_k]
+        grid = np.meshgrid(*grid_points)
         
         grid_set = []
         j = 0
-        
         for i in range(2):
             x = grid[j:j+3]
             y = np.empty(x[0].shape + (3,))
@@ -59,6 +84,11 @@ class FreeElectron:
                 eig: Eigenenergy values
                 special_x: x-coordinates of special points
         """
+        if not hasattr(self.lattice, 'reciprocal_cell') or not callable(self.lattice.reciprocal_cell.path):
+            raise AttributeError("The lattice object must have a 'reciprocal_cell' attribute with a 'path' method.")
+        if not hasattr(self.lattice, 'grid') or not callable(self.lattice.grid):
+            raise AttributeError("The lattice object must have a 'grid' method.")
+
         x, k, special_x = self.lattice.reciprocal_cell.path(n_via, *k_via)
         g = self.lattice.grid(n_g).reshape(-1, 3)
         eig = np.array([self.eigenenergy(k + gi) for gi in g])
