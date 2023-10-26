7a202263a6fe003bfebe2048b77a57b0a894508b
cohsh <82075327+cohsh@users.noreply.github.com>
2023-10-26 23:27:25 +0900

    :bug: :recycle: Zero division, Error handling

--- elphem/elph/distribution.py
+++ elphem/elph/distribution.py
@@ -7,7 +7,13 @@ from elphem.const.unit import Energy
 # Setting up system-related constants and warning filters
 float_min = sys.float_info.min
 float_max = sys.float_info.max
-warnings.simplefilter("ignore", RuntimeWarning)
+
+def safe_divide(a: np.ndarray | float | int, b: np.ndarray | float | int, default=np.nan):
+    """Safely divide two numbers, arrays, or a combination thereof."""
+    a_array = np.full_like(b, a)
+    with np.errstate(divide='ignore', invalid='ignore'):
+        result = np.divide(a_array, b, out=np.full_like(b, default), where=b != 0)
+    return result
 
 def boltzmann_distribution(temperature: float, energy: float | np.ndarray) -> float | np.ndarray:
     """
@@ -20,16 +26,11 @@ def boltzmann_distribution(temperature: float, energy: float | np.ndarray) -> fl
     Returns:
         Occupation number based on the Boltzmann distribution
     """
-    
-    kbt = temperature * Energy.KELVIN["->"]
-
-    if kbt > float_min:
-        beta = 1.0 / kbt  # inverse temperature
-    else:
-        beta = np.sqrt(float_max)
+    kbt = max(temperature * Energy.KELVIN["->"], float_min)
+    beta = safe_divide(1.0, kbt, default=float_max)
 
     ln = - beta * energy
-    return np.exp(ln)
+    return np.exp(ln, out=np.zeros_like(energy), where=ln > -np.log(float_max))
 
 def fermi_distribution(temperature: float, energy: float | np.ndarray) -> float | np.ndarray:
     """
@@ -42,9 +43,9 @@ def fermi_distribution(temperature: float, energy: float | np.ndarray) -> float 
     Returns:
         Occupation number(s) based on the Fermi-Dirac distribution.
     """
-    
     boltzmann_factor = boltzmann_distribution(temperature, energy)
-    return 1.0 / (1.0 / boltzmann_factor + 1.0)
+    inv_boltzmann_factor = safe_divide(1.0, boltzmann_factor)
+    return safe_divide(1.0, inv_boltzmann_factor + 1.0)
 
 def bose_distribution(temperature: float, energy: float | np.ndarray) -> float | np.ndarray:
     """
@@ -57,9 +58,9 @@ def bose_distribution(temperature: float, energy: float | np.ndarray) -> float |
     Returns:
         Occupation number(s) based on the Bose-Einstein distribution.
     """
-    
     boltzmann_factor = boltzmann_distribution(temperature, energy)
-    return 1.0 / (1.0 / boltzmann_factor - 1.0)
+    inv_boltzmann_factor = safe_divide(1.0, boltzmann_factor)
+    return safe_divide(1.0, inv_boltzmann_factor - 1.0)
 
 def gaussian_distribution(sigma: float, energy: float | np.ndarray) -> float | np.ndarray:
     """
@@ -72,5 +73,6 @@ def gaussian_distribution(sigma: float, energy: float | np.ndarray) -> float | n
     Returns:
         Occupation number(s) based on the Gaussian distribution.
     """
-    
+    if sigma == 0:
+        raise ValueError("Sigma must not be zero.")
     return np.exp(- energy ** 2 / (2.0 * sigma ** 2)) / (np.sqrt(2.0 * np.pi) * sigma)
