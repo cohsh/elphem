b3f7ad0092f1687940a3eb04940a16c093df9761
cohsh <82075327+cohsh@users.noreply.github.com>
2024-04-10 10:53:05 +0900

    :zap: computing couplings in SelfEnergy

--- elphem/elph/self_energy.py
+++ elphem/elph/self_energy.py
@@ -4,8 +4,7 @@ from dataclasses import dataclass
 from elphem.lattice.empty import EmptyLattice
 from elphem.electron.free import FreeElectron
 from elphem.phonon.debye import DebyeModel
-from elphem.elph.coupling import Coupling
-from elphem.elph.distribution import fermi_distribution, bose_distribution, gaussian_distribution
+from elphem.elph.distribution import fermi_distribution, bose_distribution, gaussian_distribution, safe_divide
 
 @dataclass
 class SelfEnergy:
@@ -15,6 +14,31 @@ class SelfEnergy:
     temperature: float
     sigma: float = 0.01
     eta: float = 0.01
+    effective_potential: float = 1.0 / 16.0
+    
+    def coupling(self, g1: np.ndarray, g2: np.ndarray, q: np.ndarray) -> np.ndarray:
+        """
+        Calculate lowest-order electron-phonon couplings.
+        
+        Args
+            g1, g2: A numpy array representing G-vector
+            k: A numpy array representing k-vector
+            q: A numpy array representing k-vector
+        
+        Return
+            A value of the elctron-phonon coupling.
+        """
+        q_norm = np.linalg.norm(q, axis=-1)
+        delta_g = g1 - g2
+        q_dot = np.sum(q * delta_g, axis=-1) 
+
+        mask = q_norm > 0
+        result = np.zeros_like(q_norm)
+        
+        denominator = np.sqrt(2.0 * self.phonon.mass * self.phonon.speed) * q_norm ** 1.5
+        result[mask] = safe_divide(self.effective_potential * q_dot[mask], denominator[mask])
+        
+        return result
 
     def calculate_fan_term(self, g: np.ndarray, k: np.ndarray, 
                         n_g: np.ndarray, n_q: np.ndarray) -> complex:
@@ -40,22 +64,25 @@ class SelfEnergy:
         bose = bose_distribution(self.temperature, omega)
         fermi = fermi_distribution(self.temperature, electron_energy_mkq)
 
-        coupling = Coupling.first_order(g, g_inter, q, self.phonon)
+        coupling = self.coupling(g, g_inter, q)
         
         delta_energy = electron_energy_nk - electron_energy_mkq
         # Real Part
         green_part_real = ((1.0 - fermi + bose) / (delta_energy - omega + self.eta * 1.0j)
-                           + (fermi + bose) / (delta_energy + omega + self.eta * 1.0j))
+                           + (fermi + bose) / (delta_energy + omega + self.eta * 1.0j)).real
 
         # Imaginary Part
         green_part_imag = ((1.0 - fermi + bose) * gaussian_distribution(self.sigma, delta_energy - omega)
                            + (fermi + bose) * gaussian_distribution(self.sigma, delta_energy + omega))
 
-        selfen_real = np.nansum(np.abs(coupling) ** 2 * green_part_real).real
-        selfen_imag = np.nansum(np.abs(coupling) ** 2 * green_part_imag)
+        selfen_real = np.abs(coupling) ** 2 * green_part_real
+        selfen_imag = np.abs(coupling) ** 2 * green_part_imag
+        
+        print(green_part_imag.shape)
+        print(selfen_imag.shape)
         
         coeff = 2.0 * np.pi / np.prod(n_q)
-        
+                
         return (selfen_real + 1.0j * selfen_imag) * coeff
 
     def calculate_coupling_strength(self, g: np.ndarray, k: np.ndarray,
--- tests/elph/test_self_energy.py
+++ tests/elph/test_self_energy.py
@@ -5,7 +5,7 @@ from elphem.const.unit import Mass
 from elphem.lattice.empty import EmptyLattice, LatticeConstant
 from elphem.electron.free import FreeElectron
 from elphem.phonon.debye import DebyeModel
-from elphem.elph.self_energy import SelfEnergy2nd
+from elphem.elph.self_energy import SelfEnergy
 
 class TestUnit(TestCase):
     def test_calculate(self):
@@ -22,7 +22,7 @@ class TestUnit(TestCase):
 
         temperature = debye_temperature
 
-        self_energy = SelfEnergy2nd(lattice, electron, phonon)
+        self_energy = SelfEnergy(lattice, electron, phonon, temperature)
 
         n_g = np.array([1]*3)
         n_k = np.array([5]*3)
@@ -30,6 +30,6 @@ class TestUnit(TestCase):
         
         n_g_inter = np.array([1]*3)
         n_q = np.array([5]*3)
-        selfen = self_energy.calculate(temperature, g, k, n_g_inter, n_q)
+        selfen = self_energy.calculate_fan_term(g, k, n_g_inter, n_q)
         
         self.assertEqual(selfen.shape, (n_g[0]*2, n_g[1]*2, n_g[2]*2) + (n_k[0], n_k[1], n_k[2]))
