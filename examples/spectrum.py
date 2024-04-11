1054119659242e1473e72674db777231a03342e7
cohsh <82075327+cohsh@users.noreply.github.com>
2024-04-11 18:20:44 +0900

    :zap:

--- examples/spectrum.py
+++ examples/spectrum.py
@@ -20,7 +20,7 @@ def main():
     mass = AtomicWeight.table["Li"] * Mass.DALTON["->"]
     
     debye_temperature = 344.0
-    temperature = 300.0
+    temperature = 1000.0
 
     phonon = DebyeModel(lattice, debye_temperature, 1, mass)
 
@@ -30,18 +30,18 @@ def main():
 
     n_g = np.array([1]*3)
     n_g_inter = np.array([1]*3)
-    n_q = np.array([5]*3)
-    n_omega = 200
-    range_omega = [-1.0, 2.0]
+    n_q = np.array([10]*3)
+    n_omega = 1000
+    range_omega = [-0.5, 1.5]
     
     k_names = ["G", "H", "N", "G", "P", "H"]
     k_via = [SpecialPoints.BCC[name] for name in k_names]
-    n_via = 20
+    n_via = 50
     
     x, y, spectrum, special_x = Spectrum(self_energy).calculate_with_path(n_g, k_via, n_via, n_g_inter, n_q, n_omega, range_omega)
     y_mesh, x_mesh = np.meshgrid(y, x)
 
-    ax.pcolormesh(x_mesh, y_mesh * Energy.EV["<-"], spectrum / Energy.EV["<-"])
+    mappable = ax.pcolormesh(x_mesh, y_mesh * Energy.EV["<-"], spectrum / Energy.EV["<-"])
     
     for x0 in special_x:
         ax.axvline(x=x0, color="black", linewidth=0.3)
@@ -49,7 +49,10 @@ def main():
     ax.set_xticks(special_x)
     ax.set_xticklabels(k_names)
     ax.set_ylabel("Energy ($\mathrm{eV}$)")
-    ax.set_title("Example: Band structure of bcc-Li")
+    ax.set_title("Spectral function of bcc-Li")
+    
+    # fig.colorbar(mappable, ax=ax)
+    mappable.set_clim(-2.0, 0.0)
 
     fig.savefig("test_spectrum.png")
 
