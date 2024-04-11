305b30dc49f017cd97e64c41d2d5f37927c487bc
cohsh <82075327+cohsh@users.noreply.github.com>
2024-04-11 12:20:35 +0900

    :sparkles: Compute spectrum with k-path (still in progress)

--- elphem/elph/spectrum.py
+++ elphem/elph/spectrum.py
@@ -7,7 +7,7 @@ from elphem.elph.self_energy import SelfEnergy
 class Spectrum:
     self_energy: SelfEnergy
 
-    def calculate(self, n_g: np.ndarray, n_k: np.ndarray,
+    def calculate_with_grid(self, n_g: np.ndarray, n_k: np.ndarray,
                     n_g_inter: np.ndarray, n_q: np.ndarray, n_omega: int) -> np.ndarray:
         """
         Calculate 2nd-order Fan self-energies.
@@ -52,3 +52,45 @@ class Spectrum:
             count += 1
         
         return spectrum
+    
+    def calculate_with_path(self, n_g: np.ndarray, *k_via: list[np.ndarray], n_via,
+                    n_g_inter: np.ndarray, n_q: np.ndarray, n_omega: int) -> tuple:
+        """
+        Calculate 2nd-order Fan self-energies.
+        
+        Args
+            temperature: A temperature in Kelvin.
+            n_g: A numpy array (meshgrid-type) representing G-vector
+            n_k: A numpy array (meshgrid-type) representing k-vector
+            n_g: A numpy array representing the dense of intermediate G-vectors
+            n_q: A numpy array representing the dense of intermediate q-vectors
+            
+        Return
+            A numpy array representing Fan self-energy.
+        """
+        
+        x, k, special_x = self.lattice.reciprocal_cell.path(n_via, *k_via)
+        g = self.lattice.grid(n_g).reshape(-1, 3)
+
+        epsilon = np.array([self.self_energy.eigenenergy(k + g_i) for g_i in g])
+        fan_term = np.array([self.self_energy.calculate_fan_term(g_i, k_i, n_g_inter, n_q) for g_i in g for k_i in k])
+        qp_strength = np.array([self.self_energy.calculate_qp_strength(g_i, k_i, n_g_inter, n_q) for g_i in g for k_i in k])
+
+        coeff = - qp_strength / np.pi
+        numerator = qp_strength * fan_term.imag
+        
+        omegas = np.linspace(0.0, 10.0, n_omega)
+
+        spectrum = np.zeros(fan_term.shape[0:3] + omegas.shape)
+                
+        count = 0
+        for omega in omegas:
+            denominator = (
+                (omega - epsilon - fan_term.real) ** 2
+                + (qp_strength * fan_term.imag) ** 2
+                )
+            spectrum[..., count] = np.nansum(coeff * numerator / denominator, axis=(3,4,5))
+            
+            count += 1
+        
+        return x, spectrum, special_x
--- tests/elph/test_spectrum.py
+++ tests/elph/test_spectrum.py
@@ -9,7 +9,7 @@ from elphem.elph.self_energy import SelfEnergy
 from elphem.elph.spectrum import Spectrum
 
 class TestUnit(TestCase):
-    def test_calculate(self):
+    def test_calculate_with_grid(self):
         mass = 12 * Mass.DALTON["->"]        
         temperature = 2300.0
 
@@ -27,6 +27,32 @@ class TestUnit(TestCase):
         n_q = np.array([5]*3)
         n_omega = 100
         
-        spectrum = Spectrum(self_energy).calculate(n_g, n_k, n_g_inter, n_q, n_omega)
+        spectrum = Spectrum(self_energy).calculate_with_grid(n_g, n_k, n_g_inter, n_q, n_omega)
         
         self.assertEqual(spectrum.shape, (n_g[0]*2, n_g[1]*2, n_g[2]*2) + (n_omega,))
+    
+    def test_calculate_with_path(self):
+        mass = 12 * Mass.DALTON["->"]        
+        temperature = 2300.0
+
+        lattice_constant = LatticeConstant(5,5,5,60,60,60)
+        lattice = EmptyLattice(lattice_constant)
+
+        electron = FreeElectron(lattice, 4)        
+        phonon = DebyeModel(lattice, temperature, 2, mass)
+
+        self_energy = SelfEnergy(lattice, electron, phonon, temperature)
+
+        n_g = np.array([1]*3)
+
+        k_names = ["G", "H", "N", "G", "P", "H"]
+        k_via = [SpecialPoints.BCC[name] for name in k_names]
+        n_via = 20
+
+        n_g_inter = np.array([1]*3)
+        n_q = np.array([5]*3)
+        n_omega = 100
+        
+        spectrum = Spectrum(self_energy).calculate_with_path(n_g, *k_via, n_via, n_g_inter, n_q, n_omega)
+        
+#        self.assertEqual(spectrum.shape, (n_g[0]*2, n_g[1]*2, n_g[2]*2) + (n_omega,))
