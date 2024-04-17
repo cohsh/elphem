bb408b7144aa3546963a1a3c74e85b81482ff9ad
cohsh <82075327+cohsh@users.noreply.github.com>
2024-04-17 14:20:08 +0900

    :books: added doc in distribution.py

--- elphem/elph/distribution.py
+++ elphem/elph/distribution.py
@@ -1,6 +1,5 @@
 import sys
 import numpy as np
-import warnings
 
 from elphem.const.unit import Energy
 
@@ -9,7 +8,17 @@ float_min = sys.float_info.min
 float_max = sys.float_info.max
 
 def safe_divide(a: np.ndarray | float | int, b: np.ndarray | float | int, default=np.nan):
-    """Safely divide two numbers, arrays, or a combination thereof."""
+    """
+    Safely divides two numbers, arrays, or a combination thereof, with optional handling of division by zero.
+
+    Args:
+        a (np.ndarray | float | int): Numerator, can be a number or an array.
+        b (np.ndarray | float | int): Denominator, can be a number or an array.
+        default (float | int, optional): Value to use when division by zero occurs. Default is NaN.
+
+    Returns:
+        np.ndarray: Result of the division, with division by zero handled gracefully.
+    """
     a_array = np.full_like(b, a)
     with np.errstate(divide='ignore', invalid='ignore'):
         result = np.divide(a_array, b, out=np.full_like(b, default), where=b != 0)
@@ -17,14 +26,14 @@ def safe_divide(a: np.ndarray | float | int, b: np.ndarray | float | int, defaul
 
 def boltzmann_distribution(temperature: float, energy: float | np.ndarray) -> float | np.ndarray:
     """
-    Calculates the occupation number of particles that follow the Boltzmann distribution.
+    Calculates the occupation number of particles following the Boltzmann distribution.
 
     Args:
-        temperature: Temperature in Kelvin
-        energy: Energy value(s) in Hartree atomic units
+        temperature (float): Temperature in Kelvin.
+        energy (float | np.ndarray): Energy value(s) in Hartree atomic units.
 
     Returns:
-        Occupation number based on the Boltzmann distribution
+        float | np.ndarray: Occupation number(s) based on the Boltzmann distribution.
     """
     kbt = max(temperature * Energy.KELVIN["->"], float_min)
     beta = safe_divide(1.0, kbt, default=float_max)
@@ -34,14 +43,14 @@ def boltzmann_distribution(temperature: float, energy: float | np.ndarray) -> fl
 
 def fermi_distribution(temperature: float, energy: float | np.ndarray) -> float | np.ndarray:
     """
-    Calculates the occupation number of particles that follow the Fermi-Dirac distribution.
+    Calculates the occupation number of particles following the Fermi-Dirac distribution.
 
     Args:
-        temperature: Temperature in Kelvin.
-        energy: Energy value(s) in Hartree atomic units.
+        temperature (float): Temperature in Kelvin.
+        energy (float | np.ndarray): Energy value(s) in Hartree atomic units.
 
     Returns:
-        Occupation number(s) based on the Fermi-Dirac distribution.
+        float | np.ndarray: Occupation number(s) based on the Fermi-Dirac distribution.
     """
     boltzmann_factor = boltzmann_distribution(temperature, energy)
     inv_boltzmann_factor = safe_divide(1.0, boltzmann_factor)
@@ -49,14 +58,14 @@ def fermi_distribution(temperature: float, energy: float | np.ndarray) -> float 
 
 def bose_distribution(temperature: float, energy: float | np.ndarray) -> float | np.ndarray:
     """
-    Calculates the occupation number of particles that follow the Bose-Einstein distribution.
+    Calculates the occupation number of particles following the Bose-Einstein distribution.
 
     Args:
-        temperature: Temperature in Kelvin.
-        energy: Energy value(s) in Hartree atomic units.
+        temperature (float): Temperature in Kelvin.
+        energy (float | np.ndarray): Energy value(s) in Hartree atomic units.
 
     Returns:
-        Occupation number(s) based on the Bose-Einstein distribution.
+        float | np.ndarray: Occupation number(s) based on the Bose-Einstein distribution.
     """
     boltzmann_factor = boltzmann_distribution(temperature, energy)
     inv_boltzmann_factor = safe_divide(1.0, boltzmann_factor)
@@ -64,14 +73,14 @@ def bose_distribution(temperature: float, energy: float | np.ndarray) -> float |
 
 def gaussian_distribution(sigma: float, energy: float | np.ndarray) -> float | np.ndarray:
     """
-    Calculates the occupation number of particles that follow the Gaussian distribution.
+    Calculates the probability density of particles following the Gaussian distribution.
 
     Args:
-        sigma: Standard deviation of the Gaussian distribution.
-        energy: Energy value(s) in Hartree atomic units.
+        sigma (float): Standard deviation of the Gaussian distribution.
+        energy (float | np.ndarray): Energy value(s) to evaluate the Gaussian function.
 
     Returns:
-        Occupation number(s) based on the Gaussian distribution.
+        float | np.ndarray: Probability density(s) based on the Gaussian distribution.
     """
     if sigma == 0:
         raise ValueError("Sigma must not be zero.")
