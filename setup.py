01ac99257c703fd849188f7c3d7fc3a5fffe651f
cohsh <82075327+cohsh@users.noreply.github.com>
2024-04-17 18:05:01 +0900

    :wrench: updated version-number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ AUTHOR_EMAIL = ''
 URL = 'https://github.com/cohsh/elphem'
 LICENSE = 'MIT'
 DOWNLOAD_URL = URL
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 PYTHON_REQUIRES = '>=3.10'
 INSTALL_REQUIRES = ['numpy', 'scipy']
 
