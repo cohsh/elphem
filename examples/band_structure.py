43b475a58c1ea75f1615143e65eb3d8f296f01b4
cohsh <82075327+cohsh@users.noreply.github.com>
2024-04-19 09:44:42 +0900

    :zap: updated examples

--- examples/band_structure.py
+++ examples/band_structure.py
@@ -3,24 +3,25 @@ import matplotlib.pyplot as plt
 from elphem import *
 
 def main():
-    a = 2.98 * Length.ANGSTROM["->"]
+    a = 2.98 * Length.ANGSTROM['->']
 
-    lattice = Lattice('bcc', a)
+    lattice = Lattice('bcc', a, 'Li')
     electron = FreeElectron(lattice, n_band=50, n_electron=1)
 
     k_names = ["G", "H", "N", "G", "P", "H"]
-
-    k, eig, special_k = electron.get_band_structure(k_names, n_split=20)
+    k, eig, special_k = electron.get_band_structure(k_names, n_split=40)
 
     fig, ax = plt.subplots()
     for band in eig:
         ax.plot(k, band * Energy.EV["<-"], color="tab:blue")
     
-    ax.vlines(special_k, ymin=-10, ymax=50, color="black", linewidth=0.3)
+    y_range = [-10, 50]
+
+    ax.vlines(special_k, ymin=y_range[0], ymax=y_range[1], color="black", linewidth=0.3)
     ax.set_xticks(special_k)
     ax.set_xticklabels(k_names)
     ax.set_ylabel("Energy ($\mathrm{eV}$)")
-    ax.set_ylim([-10,50])
+    ax.set_ylim(y_range)
 
     fig.savefig("example_band_structure.png")
 
