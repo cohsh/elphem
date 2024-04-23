f720682e7385b98b40d9b2a8c2efeeae449907d4
cohsh <82075327+cohsh@users.noreply.github.com>
2024-04-23 11:59:59 +0900

    :zap: splitted calculation of coupling strength

--- elphem/elph/electron_phonon.py
+++ elphem/elph/electron_phonon.py
@@ -50,7 +50,7 @@ class ElectronPhonon:
         
         return coupling
 
-    def get_self_energy_and_coupling_strength(self, g: np.ndarray, k: np.ndarray, n_q: np.ndarray) -> tuple:
+    def get_self_energy(self, omega: float, g: np.ndarray, k: np.ndarray, n_q: np.ndarray) -> tuple:
         """Calculate a single value of Fan self-energy for given wave vectors.
 
         Args:
@@ -60,41 +60,89 @@ class ElectronPhonon:
 
         Returns:
             complex: The Fan self-energy term as a complex number.
-        """        
+        """
+
         g_inter, q = self.electron.get_gk_grid(n_q) # Generate intermediate G, q grid.
 
         phonon_eigenenergy = self.phonon.get_eigenenergy(q)
         
         coefficient = 2.0 * np.pi / np.prod(n_q)
 
-        electron_eigenenergy = self.electron.get_eigenenergy(k + g)
         electron_eigenenergy_inter = self.electron.get_eigenenergy(k + g_inter + q)
 
         fermi = fermi_distribution(self.temperature, electron_eigenenergy_inter)
         bose = bose_distribution(self.temperature, phonon_eigenenergy)
 
         coupling = self.get_coupling(g, g_inter, q)
-    
-        delta_energy = electron_eigenenergy - electron_eigenenergy_inter
-    
-        # Real Part
-        green_function_real = (safe_divide(1.0 - fermi + bose, delta_energy - phonon_eigenenergy + self.eta * 1.0j)
-                            + safe_divide(fermi + bose, delta_energy + phonon_eigenenergy + self.eta * 1.0j)).real
 
-        # Imaginary Part
-        green_function_imag = ((1.0 - fermi + bose) * gaussian_distribution(self.sigma, delta_energy - phonon_eigenenergy)
-                        + (fermi + bose) * gaussian_distribution(self.sigma, delta_energy + phonon_eigenenergy))
+        occupation_absorb = 1.0 - fermi + bose
+        occupation_emit = fermi + bose
+        
+        denominator_absorb = omega - electron_eigenenergy_inter - phonon_eigenenergy
+        denominator_emit = omega - electron_eigenenergy_inter + phonon_eigenenergy
 
-        # Real Part
-        partial_green_function_real = - (safe_divide(1.0 - fermi + bose, (delta_energy - phonon_eigenenergy + self.eta * 1.0j) ** 2)
-                                    + safe_divide(fermi + bose, (delta_energy + phonon_eigenenergy + self.eta * 1.0j) ** 2)).real
+        green_function_real = (occupation_absorb * self.get_green_function_real(denominator_absorb)
+                                + occupation_emit * self.get_green_function_real(denominator_emit))
 
-        self_energy = (np.nansum(np.abs(coupling) ** 2 * green_function_real)
-                        + 1.0j * np.nansum(np.abs(coupling) ** 2 * green_function_imag)) * coefficient
+        green_function_imag = (occupation_absorb * self.get_green_function_imag(denominator_absorb)
+                                + occupation_emit * self.get_green_function_imag(denominator_emit))
+
+        self_energy = np.nansum(np.abs(coupling) ** 2 * (green_function_real + 1.0j * green_function_imag)) * coefficient
+        
+        return self_energy
+
+    def get_coupling_strength(self, omega: float, g: np.ndarray, k: np.ndarray, n_q: np.ndarray) -> np.ndarray:
+        """Calculate a single value of coupling strength for given wave vectors.
+
+        Args:
+            g (np.ndarray): G-vector in reciprocal space.
+            k (np.ndarray): k-vector of the electron state.
+            n_q (np.ndarray): Density of intermediate q-vectors for integration.
+
+        Returns:
+            complex: The Fan self-energy term as a complex number.
+        """
+
+        g_inter, q = self.electron.get_gk_grid(n_q) # Generate intermediate G, q grid.
+
+        phonon_eigenenergy = self.phonon.get_eigenenergy(q)
+        
+        coefficient = 2.0 * np.pi / np.prod(n_q)
+
+        electron_eigenenergy_inter = self.electron.get_eigenenergy(k + g_inter + q)
+
+        fermi = fermi_distribution(self.temperature, electron_eigenenergy_inter)
+        bose = bose_distribution(self.temperature, phonon_eigenenergy)
+
+        coupling = self.get_coupling(g, g_inter, q)
+
+        occupation_absorb = 1.0 - fermi + bose
+        occupation_emit = fermi + bose
+        
+        denominator_absorb = omega - electron_eigenenergy_inter - phonon_eigenenergy
+        denominator_emit = omega - electron_eigenenergy_inter + phonon_eigenenergy
+
+        partial_green_function_real = (occupation_absorb * self.get_partial_green_function_real(denominator_absorb)
+                                       + occupation_emit * self.get_partial_green_function_real(denominator_emit))
 
         coupling_strength = - np.nansum(np.abs(coupling) ** 2 * partial_green_function_real) * coefficient
         
-        return self_energy, coupling_strength
+        return coupling_strength
+
+    def get_green_function_real(self, omega: np.ndarray) -> np.ndarray:
+        green_function_real = safe_divide(1.0, omega + self.eta * 1.0j).real
+        
+        return green_function_real
+    
+    def get_green_function_imag(self, omega: np.ndarray) -> np.ndarray:
+        green_function_imag = gaussian_distribution(self.sigma, omega)
+        
+        return green_function_imag
+
+    def get_partial_green_function_real(self, omega: np.ndarray) -> np.ndarray:
+        partial_green_function_real = -1.0 * safe_divide(1.0, (omega + self.eta * 1.0j) ** 2).real
+        
+        return partial_green_function_real
 
     @staticmethod
     def get_qp_strength(coupling_strength: np.ndarray) -> np.ndarray:
