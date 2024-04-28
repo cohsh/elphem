bde3b886da6b337e3d4bc50d94380da9a46889e7
cohsh <82075327+cohsh@users.noreply.github.com>
2024-04-28 15:26:39 +0900

    :zap: updated examples

--- examples/spectrum_k.py
+++ examples/spectrum_k.py
@@ -8,14 +8,14 @@ def main():
     debye_temperature = 344.0
 
     lattice = Lattice('bcc', 'Li', a)
-    electron = FreeElectron(lattice, n_band=2, n_electron=1)
+    electron = FreeElectron(lattice, n_band=1, n_electron=1)
     phonon = DebyePhonon(lattice, debye_temperature)
 
-    n_q = np.full(3, 25)
+    n_q = np.full(3, 30)
     temperature = debye_temperature
 
-    n_omega = 1000
-    range_omega = [1. * Energy.EV["->"], 1.8 * Energy.EV["->"]]
+    n_omega = 2000
+    range_omega = [0.5 * Energy.EV["->"], 2.5 * Energy.EV["->"]]
     
     elph = ElectronPhonon(electron, phonon, temperature, n_q, sigma=0.00001)
     
@@ -30,7 +30,7 @@ def main():
     ax.plot(omega * Energy.EV["<-"], np.abs(spectrum[0]) / Energy.EV["<-"])
 #    ax.axvline(x=0.0, color="black", linewidth=0.3)
 
-    ax.set_ylim(0,10)
+    ax.set_ylim(0,30)
     ax.set_xlabel("$\omega$ ($\mathrm{eV}$)")
     ax.set_ylabel("$A(\mathbf{k}_\mathrm{N}, \omega)$ ($\mathrm{eV}^{-1}$)")
     ax.set_title("Spectral function of bcc-Li")
