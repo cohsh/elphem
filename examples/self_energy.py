7d979dd3689d6c83879708684e58adbd103f67c3
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-03 12:14:38 +0900

    :zap: examples/ | updated self_energy

--- examples/self_energy.py
+++ examples/self_energy.py
@@ -7,16 +7,13 @@ def main():
     a = 2.98 * Length.ANGSTROM['->']
     debye_temperature = 344.0
     n_q = np.full(3, 20)
-    k_names = ["G", "H", "N", "G", "P", "H"]
-    n_split = 40
     n_electron = 1
     n_band = 1
+    k_gamma = np.full(3, 0.)
 
     lattice = Lattice('bcc', 'Li', a, debye_temperature)
 
-    k_path = lattice.reciprocal.get_path(k_names, n_split)
-
-    electron = FreeElectron.create_from_k(lattice, n_electron, n_band, k_path.values)
+    electron = FreeElectron.create_from_k(lattice, n_electron, n_band, k_gamma)
     phonon = DebyePhonon.create_from_n(lattice, debye_temperature, n_q)
 
     electron_phonon = ElectronPhonon(electron, phonon)
@@ -27,25 +24,17 @@ def main():
     
     self_energies = electron_phonon.calculate_self_energies_over_range(omega_array)
     
-    y, x = np.meshgrid(omega_array, k_path.minor_scales)
-
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    
-    mappable = ax.pcolormesh(x, y * Energy.EV["<-"], self_energies * Energy.EV["<-"])
-    
-    for x0 in k_path.major_scales:
-        ax.axvline(x=x0, color="black", linewidth=0.3)
-    
-    ax.set_xticks(k_path.major_scales)
-    ax.set_xticklabels(k_names)
-    ax.set_ylabel("Energy ($\mathrm{eV}$)")
-    ax.set_title("Spectral function of bcc-Li")
-    
-    fig.colorbar(mappable, ax=ax)
-    mappable.set_clim(-10.0, 0.0)
 
-    fig.savefig("spectrum.png")
+    ax.plot(omega_array * Energy.EV["<-"], self_energies[0, 0] * Energy.EV["<-"])
+
+    ax.set_ylim(0,30)
+    ax.set_xlabel("$\omega$ ($\mathrm{eV}$)")
+    ax.set_ylabel("$A(\mathbf{k}_\mathrm{N}, \omega)$ ($\mathrm{eV}^{-1}$)")
+    ax.set_title("Spectral function of bcc-Li")
+
+    fig.savefig("self_energy_k.png")
 
 if __name__ == "__main__":
     main()
