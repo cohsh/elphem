bcf5af208f2a34c51bd572fa77c7df7cf70d0a5e
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-04 00:15:56 +0900

    :zap: examples/spectrum_k | replace: k_path -> k

--- examples/spectrum_k.py
+++ examples/spectrum_k.py
@@ -6,32 +6,31 @@ def main():
     # Example: Li (BCC)
     a = 2.98 * Length.ANGSTROM["->"]
     debye_temperature = 344.0
-    n_band = 1
     n_electron = 1
-    k_names = ["N", "H"]
-    n_split = 2
+    n_band = 2
     n_q_array = np.full(3, 30)
 
     lattice = Lattice('bcc', 'Li', a, debye_temperature)
+
+    k = lattice.reciprocal.calculate_special_k('N')
     
-    k_path = lattice.reciprocal.get_path(k_names, n_split)
-    electron = FreeElectron.create_from_path(lattice, n_electron, n_band, k_path)
+    electron = FreeElectron.create_from_k(lattice, n_electron, n_band, k)
     phonon = DebyePhonon.create_from_n(lattice, debye_temperature, n_q_array)
 
-    n_omega = 20000
-    range_omega = [1.0 * Energy.EV["->"], 2. * Energy.EV["->"]]
+    n_omega = 3000
+    range_omega = [0.5 * Energy.EV["->"], 2.0 * Energy.EV["->"]]
     omega_array = np.linspace(range_omega[0], range_omega[1], n_omega)
     
-    elph = ElectronPhonon(electron, phonon)
+    electron_phonon = ElectronPhonon(electron, phonon)
     
-    spectrum = elph.calculate_spectrum_over_range(omega_array)
+    spectrum = electron_phonon.calculate_spectrum_over_range(omega_array)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
     ax.plot(omega_array * Energy.EV["<-"], np.abs(spectrum[0]) / Energy.EV["<-"])
 
-    ax.set_ylim(0,200)
+#    ax.set_ylim(0,200)
     ax.set_xlabel("$\omega$ ($\mathrm{eV}$)")
     ax.set_ylabel("$A(\mathbf{k}_\mathrm{N}, \omega)$ ($\mathrm{eV}^{-1}$)")
     ax.set_title("Spectral function of bcc-Li")
