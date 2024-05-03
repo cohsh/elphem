6670d6989abc39cf9504d379553ecc08f22192f3
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-04 00:59:34 +0900

    :zap: examples/spectrum_k | changed parameters

--- examples/spectrum_k.py
+++ examples/spectrum_k.py
@@ -17,8 +17,8 @@ def main():
     electron = FreeElectron.create_from_k(lattice, n_electron, n_band, k)
     phonon = DebyePhonon.create_from_n(lattice, debye_temperature, n_q_array)
 
-    n_omega = 3000
-    range_omega = [0.5 * Energy.EV["->"], 2.0 * Energy.EV["->"]]
+    n_omega = 10000
+    range_omega = [1.0 * Energy.EV["->"], 1.5 * Energy.EV["->"]]
     omega_array = np.linspace(range_omega[0], range_omega[1], n_omega)
     
     electron_phonon = ElectronPhonon(electron, phonon)
@@ -30,7 +30,7 @@ def main():
 
     ax.plot(omega_array * Energy.EV["<-"], np.abs(spectrum[0]) / Energy.EV["<-"])
 
-#    ax.set_ylim(0,200)
+    ax.set_ylim(0,50)
     ax.set_xlabel("$\omega$ ($\mathrm{eV}$)")
     ax.set_ylabel("$A(\mathbf{k}_\mathrm{N}, \omega)$ ($\mathrm{eV}^{-1}$)")
     ax.set_title("Spectral function of bcc-Li")
