94fbaceeb1469a7e3bdb1fb4aa20153d1952a2bd
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-04 12:08:46 +0900

    :zap: examples/ | add spectrum near Fermi surface

---
+++ examples/spectrum_near_fermi.py
@@ -0,0 +1,52 @@
+"""Example: bcc-Li"""
+import numpy as np
+import matplotlib.pyplot as plt
+from elphem import *
+
+def main():
+    a = 2.98 * Length.ANGSTROM['->']
+    debye_temperature = 344.0
+    n_q = np.full(3, 15)
+    k_names = ["N", "G"]
+    n_split = 400
+    n_electron = 1
+    n_band = 1
+
+    lattice = Lattice('bcc', 'Li', a, debye_temperature * 0.5)
+
+    k_path = lattice.reciprocal.get_path(k_names, n_split)
+
+    electron = FreeElectron.create_from_path(lattice, n_electron, n_band, k_path)
+    phonon = DebyePhonon.create_from_n(lattice, debye_temperature, n_q)
+
+    electron_phonon = ElectronPhonon(electron, phonon)
+
+    n_omega = 200
+    range_omega = [-0.035 * Energy.EV["->"], 0.035 * Energy.EV["->"]]
+    omega_array = np.linspace(range_omega[0] , range_omega[1], n_omega)
+    
+    spectrum = electron_phonon.calculate_spectrum_over_range(omega_array)
+    
+    y, x = np.meshgrid(omega_array, k_path.minor_scales)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    mappable = ax.pcolormesh(x, y * Energy.EV["<-"], spectrum / Energy.EV["<-"])
+    
+#    for x0 in k_path.major_scales:
+#        ax.axvline(x=x0, color="black", linewidth=0.3)
+    
+#    ax.set_xticks(k_path.major_scales)
+#    ax.set_xticklabels(k_names)
+    ax.set_xlim([0.05, 0.125])
+    ax.set_ylabel("Energy ($\mathrm{eV}$)")
+    ax.set_title("Spectral function of bcc-Li")
+    
+    fig.colorbar(mappable, ax=ax)
+    mappable.set_clim(-5.0, 0.0)
+
+    fig.savefig("spectrum_near_fermi.png")
+
+if __name__ == "__main__":
+    main()
