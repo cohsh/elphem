018ac38a483ac9b71e8adec642d8112894ac5709
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-06 14:47:31 +0900

    :zap: added Lattice2D and 1D

--- elphem/lattice/lattice.py
+++ elphem/lattice/lattice.py
@@ -3,9 +3,9 @@ import numpy as np
 
 from elphem.common.atomic_weight import AtomicWeight
 from elphem.common.unit import Mass
-from elphem.lattice.primitive_cell import PrimitiveCell3D
-from elphem.lattice.reciprocal_cell import ReciprocalCell3D
-from elphem.lattice.lattice_constant import LatticeConstant3D
+from elphem.lattice.primitive_cell import PrimitiveCell3D, PrimitiveCell2D, PrimitiveCell1D
+from elphem.lattice.reciprocal_cell import ReciprocalCell3D, ReciprocalCell2D, ReciprocalCell1D
+from elphem.lattice.lattice_constant import LatticeConstant3D, LatticeConstant2D, LatticeConstant1D
 
 @dataclass
 class Lattice:
@@ -39,6 +39,7 @@ class Lattice3D(Lattice):
     def __init__(self, crystal_structure: str, atoms: str | list[str] | np.ndarray, a: float, temperature: float = None):
         """Initializes the lattice constants, primitive, and reciprocal cells along with their volumes and bases."""
         super().__init__(crystal_structure, atoms, a, temperature)
+        self.n_dim = 3
         self.set_constants()
 
         self.primitive = PrimitiveCell3D(self.constants)
@@ -67,3 +68,58 @@ class Lattice3D(Lattice):
             self.constants = LatticeConstant3D(self.a, self.a, self.a, alpha, alpha, alpha, crystal_structure_lower)
         else:
             raise ValueError("Invalid crystal structure specified.")
+
+class Lattice2D(Lattice):
+    def __init__(self, crystal_structure: str, atoms: str | list[str] | np.ndarray, a: float, temperature: float = None):
+        """Initializes the lattice constants, primitive, and reciprocal cells along with their volumes and bases."""
+        super().__init__(crystal_structure, atoms, a, temperature)
+        self.n_dim = 2
+        self.set_constants()
+
+        self.primitive = PrimitiveCell2D(self.constants)
+        self.reciprocal = ReciprocalCell2D(self.constants)
+
+    def set_constants(self) -> None:
+        """Determines lattice constants based on the crystal structure.
+
+        Returns:
+            LatticeConstant: The lattice constants and angles for the specified crystal structure.
+
+        Raises:
+            ValueError: If an invalid crystal structure name is specified.
+        """
+        crystal_structure_lower = self.crystal_structure.lower()
+
+        alpha_values = {
+            'square': 90.0,
+            'hexagonal': 120.0
+        }
+
+        alpha = alpha_values.get(crystal_structure_lower)
+
+        if alpha is not None:
+            self.constants = LatticeConstant2D(self.a, self.a, alpha, crystal_structure_lower)
+        else:
+            raise ValueError("Invalid crystal structure specified.")
+
+class Lattice1D(Lattice):
+    def __init__(self, atoms: str | list[str] | np.ndarray, a: float, temperature: float = None):
+        """Initializes the lattice constants, primitive, and reciprocal cells along with their volumes and bases."""
+        super().__init__('', atoms, a, temperature)
+        self.n_dim = 1
+        self.set_constants()
+
+        self.primitive = PrimitiveCell1D(self.constants)
+        self.reciprocal = ReciprocalCell1D(self.constants)
+
+    def set_constants(self) -> None:
+        """Determines lattice constants based on the crystal structure.
+
+        Returns:
+            LatticeConstant: The lattice constants and angles for the specified crystal structure.
+
+        Raises:
+            ValueError: If an invalid crystal structure name is specified.
+        """
+
+        self.constants = LatticeConstant1D(self.a)
