a22f49c7fcc55f233b813bde5b1c99efc67ab836
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-08 15:06:26 +0900

    :zap: examples/ | changed parameters

--- examples/1D/self_energy.py
+++ examples/1D/self_energy.py
@@ -7,7 +7,7 @@ from elphem import *
 def main():
     a = 2.98 * Length.ANGSTROM['->']
     debye_temperature = 344.0
-    n_q = np.full(1, 50)
+    n_q = np.full(1, 500)
     n_electron = 1
     n_band = 1
 
@@ -19,7 +19,7 @@ def main():
     phonon = DebyePhonon.create_from_n(lattice, debye_temperature, n_q)
 
     n_omega = 10000
-    range_omega = [-10 * Energy.EV["->"], 4 * Energy.EV["->"]]
+    range_omega = [-10 * Energy.EV["->"], 20 * Energy.EV["->"]]
     omega_array = np.linspace(range_omega[0] , range_omega[1], n_omega)
 
     electron_phonon = ElectronPhonon(electron, phonon, sigma=0.001, eta=0.01)
@@ -32,15 +32,12 @@ def main():
 
     ax['real'].plot(omega_array * Energy.EV["<-"], self_energies[0, 0].real * Energy.EV["<-"] * 1000.0)
     ax['imag'].plot(omega_array * Energy.EV["<-"], self_energies[0, 0].imag * Energy.EV["<-"] * 1000.0)
-    ax['real'].plot(omega_array * Energy.EV["<-"], (omega_array - electron.eigenenergies[0, 0]) * Energy.EV["<-"] * 1000.0 )
 
     ax['imag'].set_xlabel("$\omega$ ($\mathrm{eV}$)")
     ax['real'].set_ylabel("$\mathrm{Re}\Sigma_{n=0}(\mathbf{k}_\Gamma, \omega)$ ($\mathrm{meV}$)")
     ax['imag'].set_ylabel("$\mathrm{Im}\Sigma_{n=0}(\mathbf{k}_\Gamma, \omega)$ ($\mathrm{meV}$)")
 
     ax['real'].set_title("Self-energy of bcc-Li")
-
-    ax['real'].set_ylim([np.nanmin(self_energies[0,0].real * Energy.EV['<-'] * 1000.0) * 1.1, np.nanmax(self_energies[0,0].real * Energy.EV['<-'] * 1000.0) * 1.1])
 
     fig.savefig("self_energy.png")
 
