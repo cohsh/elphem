9f587ace860dbafa2a3124971d70e53dbd625c41
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-11 16:41:21 +0900

    :zap: changed parameters

--- examples/1D/spectrum_near_fermi.py
+++ examples/1D/spectrum_near_fermi.py
@@ -12,17 +12,17 @@ def main():
     n_electron = 1
     n_band = 2
 
-    lattice = Lattice1D('Li', a, 5.0)
+    lattice = Lattice1D('Li', a, 10.0)
 
     k_path = lattice.reciprocal.get_path(k_names, n_split)
 
     electron = FreeElectron.create_from_path(lattice, n_electron, n_band, k_path)
     phonon = DebyePhonon.create_from_n(lattice, debye_temperature, n_q)
 
-    electron_phonon = ElectronPhonon(electron, phonon, sigma=0.0001, eta=0.1)
+    electron_phonon = ElectronPhonon(electron, phonon, sigma=0.0001, eta=0.005)
 
     n_omega = 1000
-    range_omega = [-0.4 * Energy.EV["->"], 0.4 * Energy.EV["->"]]
+    range_omega = [-0.5 * Energy.EV["->"], 0.5 * Energy.EV["->"]]
     omega_array = np.linspace(range_omega[0] , range_omega[1], n_omega)
     
     spectrum = electron_phonon.calculate_spectrum_over_range(omega_array)
@@ -43,7 +43,7 @@ def main():
     ax.set_title("Spectral function of bcc-Li")
     
     fig.colorbar(mappable, ax=ax)
-    mappable.set_clim(-30.0, 0.0)
+    mappable.set_clim(-30.0, -10.0)
 
     fig.savefig("spectrum_near_fermi.png")
 
