fc9d3ff555d01b3f2c2606c577fac45ab1add86e
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-15 17:39:21 +0900

    :zap: examples

--- examples/3D/spectrum_near_fermi.py
+++ examples/3D/spectrum_near_fermi.py
@@ -6,23 +6,24 @@ from elphem import *
 def main():
     a = 2.98 * Length.ANGSTROM['->']
     debye_temperature = 344.0
-    n_q = np.full(3, 20)
-    k_names = ["0.5N", "N"]
-    n_split = 150
+    n_q = np.full(3, 10)
+    k_names = ["0.7N", "H"]
+    n_split = 100
+    n_omega = 100
     n_electron = 1
     n_band = 1
+    temperature = 50.0
 
-    lattice = Lattice3D('bcc', 'Li', a, 10.0)
+    lattice = Lattice3D('bcc', 'Li', a)
 
     k_path = lattice.reciprocal.get_path(k_names, n_split)
 
     electron = FreeElectron.create_from_path(lattice, n_electron, n_band, k_path)
     phonon = DebyePhonon.create_from_n(lattice, debye_temperature, n_q)
 
-    electron_phonon = ElectronPhonon(electron, phonon, sigma=0.0001, eta=0.0001)
+    electron_phonon = ElectronPhonon(electron, phonon, temperature, sigma=0.001, eta=0.0005)
 
-    n_omega = 100
-    range_omega = [-0.1 * Energy.EV["->"], 0.1 * Energy.EV["->"]]
+    range_omega = [-2.0 * Energy.EV["->"], 0.0 * Energy.EV["->"]]
     omega_array = np.linspace(range_omega[0] , range_omega[1], n_omega)
     
     spectrum = electron_phonon.calculate_spectrum_over_range(omega_array)
@@ -34,6 +35,11 @@ def main():
     
     mappable = ax.pcolormesh(x, y * Energy.EV["<-"], spectrum / Energy.EV["<-"])
     
+    ax.plot(k_path.minor_scales, electron_phonon.eigenenergies[0] * Energy.EV['<-'], linestyle='dashed', color='tab:gray')
+        
+    ax.set_ylim([range_omega[0] * Energy.EV['<-'], range_omega[1] * Energy.EV['<-']])
+
+    
     for x0 in k_path.major_scales:
         ax.axvline(x=x0, color="black", linewidth=0.3)
     
@@ -43,7 +49,7 @@ def main():
     ax.set_title("Spectral function of bcc-Li")
     
     fig.colorbar(mappable, ax=ax)
-    mappable.set_clim(0.0, 1.0)
+    mappable.set_clim(0.0, 0.01)
 
     fig.savefig("spectrum_near_fermi.png")
 
