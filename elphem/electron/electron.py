38d35ee33afa4d2325f8b239cf94d2986def77c3
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-16 18:15:23 +0900

    :zap: Electron | docstring

--- elphem/electron/electron.py
+++ elphem/electron/electron.py
@@ -5,10 +5,10 @@ from elphem.lattice.lattice import Lattice
 from elphem.lattice.path import PathValues
 
 class Electron:
-    """Represents a free electron in a given empty lattice.
+    """Represents free electrons in a given empty lattice.
     
     Attributes:
-        lattice (Lattice): The lattice on which the free electron model is applied.
+        lattice (Lattice): Lattice on which the free electron model is applied.
         n_electrons (int): Number of electrons per unit cell.
         n_bands (int): Number of energy bands considered.
         n_k (int): Number of k_points.
@@ -19,6 +19,12 @@ class Electron:
     """
     
     def __init__(self, lattice: Lattice, n_electrons: int):
+        """
+        Args:
+            lattice (Lattice): Lattice on which the free electron model is applied.
+            n_electrons (int): Number of electrons per unit cell.
+        """
+        
         self.lattice = lattice
         self.n_electrons = n_electrons
         self.n_bands = None
@@ -31,11 +37,24 @@ class Electron:
         
     @classmethod
     def create_from_n(cls, lattice: Lattice, n_electrons: int, n_bands: int, n_k_array: np.ndarray) -> 'Electron':
+        """Create free electrons from the number of the number of k (array-type).
+
+        Args:
+            lattice (Lattice): Lattice on which the free electron model is applied
+            n_electrons (int): Number of electrons
+            n_bands (int): Number of electron bands
+            n_k_array (np.ndarray): Number of k vectors (array-type)
+
+        Returns:
+            Electron: Free electron
+        """
         electron = Electron(lattice, n_electrons)
 
+        # set about k vectors
         electron.k = lattice.reciprocal.get_monkhorst_pack_grid(*n_k_array)
         electron.n_k = np.prod(n_k_array)
 
+        # update values
         electron.update_band(n_bands)
         electron.update_eigenenergies()
         
@@ -43,17 +62,31 @@ class Electron:
 
     @classmethod
     def create_from_k(cls, lattice: Lattice, n_electrons: int, n_bands: int, k_array: np.ndarray) -> 'Electron':
+        """Create free electrons from k vectors.
+
+        Args:
+            lattice (Lattice): Lattice on which the free electron model is applied
+            n_electrons (int): Number of electrons
+            n_bands (int): Number of electron bands
+            k_array (np.ndarray): k vectors
+
+        Returns:
+            Electron: Free electron
+        """
         electron = Electron(lattice, n_electrons)
 
+        # check the type and shape fo k_array
         if isinstance(k_array, list):
             k_array = np.array(k_array)
 
         if k_array.shape == (lattice.n_dim,):
             k_array = np.array([k_array])
 
+        # set about k vectors
         electron.n_k = len(k_array)
         electron.k = k_array
-        
+
+        # update values
         electron.update_band(n_bands)
         electron.update_eigenenergies()
         
@@ -61,6 +94,17 @@ class Electron:
 
     @classmethod
     def create_from_gk_grid(cls, lattice: Lattice, n_electrons: int, g_array: np.ndarray, k_array: np.ndarray) -> 'Electron':
+        """Create free electrons from G-k grid.
+
+        Args:
+            lattice (Lattice): Lattice on which the free electron model is applied
+            n_electrons (int): Number of electrons
+            g_array (np.ndarray): Reciprocal lattice vectors
+            k_array (np.ndarray): k vectors
+
+        Returns:
+            Electron: Free electron
+        """
         electron = Electron(lattice, n_electrons)
         
         electron.g = g_array
@@ -72,12 +116,34 @@ class Electron:
     
     @classmethod
     def create_from_path(cls, lattice: Lattice, n_electrons: int, n_bands: int, k_path: PathValues) -> 'Electron':
+        """Create free electrons from k-path.
+
+        Args:
+            lattice (Lattice): Lattice on which the free electron model is applied
+            n_electrons (int): Number of electrons
+            n_bands (int): Number of bands
+            k_path (PathValues): Path in the reciprocal lattice
+
+        Returns:
+            Electron: Free electron
+        """
         electron = Electron.create_from_k(lattice, n_electrons, n_bands, k_path.values)
         
         return electron
 
     def clone_with_gk_grid(self, g_array: np.ndarray, k_array: np.ndarray) -> 'Electron':
+        """Clone a free electron with changing G and k vectors.
+
+        Args:
+            g_array (np.ndarray): Reciprocal lattice vectors
+            k_array (np.ndarray): k vectors
+
+        Returns:
+            Electron: Free electron
+        """
         electron = self.create_from_gk_grid(self.lattice, self.n_electrons, g_array, k_array)
+
+        # set about numbers
         electron.n_k = self.n_k
         electron.n_bands = self.n_bands
         
@@ -87,11 +153,11 @@ class Electron:
         """Calculate the electron eigenenergies at wave vector k.
         
         Args:
-            k_array (Optional[np.ndarray]): A numpy array representing vectors in reciprocal space.
-            g_array (Optional[np.ndarray]): An optional numpy array of vectors to be added to `k_array`.
+            k_array (np.ndarray): k vectors
+            g_array (np.ndarray): Reciprocal lattice vectors
 
         Returns:
-            np.ndarray: The electron eigenenergies at each wave vector.
+            np.ndarray: The electron eigenenergies
         """
         if g_array is None:
             eigenenergies =  0.5 * np.linalg.norm(k_array, axis=-1) ** 2 - self.fermi_energy
@@ -102,6 +168,14 @@ class Electron:
         return eigenenergies
 
     def calculate_dos(self, omega: float | np.ndarray) -> np.ndarray:
+        """Calculate DOS by using analytical forms.
+
+        Args:
+            omega (float | np.ndarray): frequencies
+
+        Returns:
+            np.ndarray: DOS
+        """
         omega_plus_fermi_energy = omega + self.fermi_energy
         if self.lattice.n_dim == 3:
             coefficient = 8.0 * np.pi / self.lattice.reciprocal.volume
@@ -113,16 +187,21 @@ class Electron:
             coefficient = 2.0 / self.lattice.reciprocal.volume
             return coefficient / np.sqrt(2.0 * omega_plus_fermi_energy)
 
-    def calculate_eigenenergies_with_path(self, k_path: PathValues) -> PathValues:
-        eigenenergies = self.calculate_eigenenergies(k_path.values, self.g)
-        
-        return k_path.derive(eigenenergies)
-
     def update_band(self, n_bands: int) -> None:
+        """Update attributes about bands
+
+        Args:
+            n_bands (int): Number of bands
+        """
         self.n_bands = n_bands
         self.g = self.lattice.reciprocal.get_reciprocal_vectors(self.n_bands)
 
     def update_eigenenergies(self, expand_g: bool = True) -> None:
+        """Update eigenenergies
+
+        Args:
+            expand_g (bool, optional): add axis about reciprocal lattice vectors. Defaults to True.
+        """
         if expand_g:
             self.eigenenergies = self.calculate_eigenenergies(self.k, self.g)
         else:
